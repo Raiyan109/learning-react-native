8bfc5d498328bdf4897cd97129c0dd481c1b6e05
Raiyan109 <kraiyan109@gmail.com>
2024-05-31 12:38:22 +0600

    Image

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
-const index = () => {
+const bg = require('../assets/images/background-image.png')
+const App = () => {
+
     return (
         <View style={styles.container}>
-            <Text>RootLayout</Text>
+            <Text className='text-4xl text-purple-400'>Raiyan Kabir and Habiba </Text>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={bg}
+                    style={styles.image}
+                />
+            </View>
             <Link href={'/profile'} style={{ color: 'red' }}>Go to Profile</Link>
+            <StatusBar style='light' backgroundColor='purple' />
         </View>
     )
 }
 
-export default index
+export default App
 
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        justifyContent: 'center',
+        backgroundColor: '#25292e',
+        color: '#fff'
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
+    },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
 }) 
